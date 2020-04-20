dc92d433c3820a6a1dcc0b46a0a9094fbfbd814d
Henrique Barbosa <mudgenone@gmail.com>
2020-04-19 23:45:32 -0300

    feat: add create address

--- src/adresses/adresses.service.ts
+++ src/adresses/adresses.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Address } from './address.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateAddressDto } from './dto/create-address.dto';
 
 @Injectable()
-export class AdressesService {}
+export class AdressesService {
+  constructor(
+    @InjectRepository(Address)
+    private adressesRepository: Repository<Address>,
+  ) {}
+
+  async create(createAddressDto: CreateAddressDto) {
+    const address = this.adressesRepository.create(createAddressDto);
+    await address.save();
+  }
+}
---
+++ src/adresses/dto/create-address.dto.ts
@@ -0,0 +1,8 @@
+export class CreateAddressDto {
+  postalCode: string;
+  country: string;
+  state: string;
+  city: string;
+  neighborhood: string;
+  street: string;
+}
